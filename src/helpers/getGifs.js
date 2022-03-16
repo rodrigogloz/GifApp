a0c49465c0eca24085a67fd41103f1e6a8aeda75
Rodrigo Gloz <rodrigo.ezequiel.gloz@gmail.com>
2022-03-16 13:41:58 +0100

    Primer commit antes de modificar estilos para reorganizar el gif grid

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}}&limit=20&api_key=4P17iao9iE2TbSjA4p4N7cv0rOIVajjw`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url,
+    };
+  });
+
+  return gifs;
+};
