a9614554f9b995e4900bdcff5bb2b8088cdad78b
Rodrigo Gloz <rodrigo.ezequiel.gloz@gmail.com>
2022-06-11 14:36:44 +0200

    Allow deleting one single row

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,12 +3,14 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["Cat memes"]);
+  const [categories, setCategories] = useState(["Cats", "Coding", "Synths"]);
 
   return (
     <div className="app-container">
       <header className="app-header">
-        <h2 className="header-title">GifExpertApp</h2>
+        <h2 className="header-title animate__animated animate__fadeIn">
+          GifExpertApp
+        </h2>
         <AddCategory setCategories={setCategories} />
         {/* <hr className="header-hr" /> */}
       </header>
