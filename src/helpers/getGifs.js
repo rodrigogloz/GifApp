bf38034e2df313e317bd4cce7b2027af3cb6c59e
Rodrigo Gloz <rodrigo.ezequiel.gloz@gmail.com>
2022-08-09 15:19:12 +0200

    Modified gif fetch quantity

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@ import React from "react";
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
-  )}}&limit=${20}&api_key=4P17iao9iE2TbSjA4p4N7cv0rOIVajjw`;
+  )}}&limit=${25}&api_key=4P17iao9iE2TbSjA4p4N7cv0rOIVajjw`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
