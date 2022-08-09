3fc41a475b7528f6403d157e40ade8844da6c3e8
Rodrigo Gloz <rodrigo.ezequiel.gloz@gmail.com>
2022-08-09 15:05:47 +0200

    Update footer with external links

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
+import GitHub from "./assets/github-svgrepo-com .svg";
+import Linkedin from "./assets/linkedin-svgrepo-com.svg";
 
 const GifExpertApp = () => {
   const [categories, setCategories] = useState(["Cats", "Coding", "Synths"]);
@@ -9,7 +11,7 @@ const GifExpertApp = () => {
     <div className="app-container">
       <header className="app-header">
         <h2 className="header-title animate__animated animate__fadeIn">
-          GifExpertApp
+          Gif Finder
         </h2>
         <AddCategory setCategories={setCategories} />
         {/* <hr className="header-hr" /> */}
@@ -20,7 +22,15 @@ const GifExpertApp = () => {
         ))}
       </ol>
       <footer className="app-footer">
-        <div className="app-footer-content"></div>
+        <div className="app-footer-content">
+          <a href="https://github.com/rodrigogloz" target="_blank">
+            <img className="footer-logo" src={GitHub} alt="GitHub" />
+          </a>
+          <p>© 2022 Rodrigo Ezequiel Gloz</p>
+          <a href="https://www.linkedin.com/in/rodrigo-gloz/" target="_blank">
+            <img className="footer-logo" src={Linkedin} alt="Linkedin" />
+          </a>
+        </div>
       </footer>
     </div>
   );
